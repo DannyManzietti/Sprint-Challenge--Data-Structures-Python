66c7a5ce234f76aae5e098a7439ba4a71c88c1c8
Danny Manzietti <dannymanzietti@Dannys-MacBook-Pro.local>
2020-02-28 12:02:27 -0500

    ring buffer i believe passes but am getting error about stretch array ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,15 +8,40 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # check to see if list is at capacity
+        # if it is
+        if self.capacity == len(self.storage):
+            # replace current with new item
+            self.current.value = item
+
+            # move to next item in list
+            # if current item is tail
+            if self.current == self.storage.tail:
+                # next is head value
+                self.current = self.storage.head
+            else:
+                self.current = self.current.next
+        # not at capacity
+        else:
+            # add to end storage
+            self.storage.add_to_tail(item)
+            # if no items
+            if len(self.storage) == 1:
+                # set to new item
+                self.current = self.storage.head
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        node = self.storage.head
+        while node is not None:
+            # add value to list
+            list_buffer_contents.append(node.value)
+            node = node.next
 
         return list_buffer_contents
+
 
 # ----------------Stretch Goal-------------------
 
